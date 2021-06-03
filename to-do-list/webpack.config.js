68c0d75e91ce4de3502f8612f3b3b8ff66b34973
notReally08 <49450138+notReally08@users.noreply.github.com>
2021-06-03 11:16:26 +0300

    ToDo list: Basic configuration (#13)

---
+++ to-do-list/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = (env, argv) => {
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000,
+    },
+    mode: argv.mode === 'development' ? 'development' : 'production',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html'
+      }),
+
+    ],
+  }
+}
